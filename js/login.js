861fa2aea11286962bfe835b08229ab101298fb7
abidscode <mh.abid.2003@gmail.com>
2024-11-07 21:50:15 +0600

    home page desinged

--- js/login.js
+++ js/login.js
@@ -1,8 +1,27 @@
+// document.getElementById('btn-login')
+//     .addEventListener('click', function (event){
+//         event.preventDefault();
+//         console.log('Login Button Clicked');
+
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         console.log(phoneNumber);
+
+
+//     })
+
 document.getElementById('btn-login')
-    .addEventListener('click', function (event){
+    .addEventListener('click', function(event){
         event.preventDefault();
-        console.log('Login Button Clicked');
 
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById('pin-number').value;
+
+        if(phoneNumber === '01600376406' && pinNumber === '340143'){
+            console.log('Welcome! You are logged in');
+            window.location.href = '/home.html';
+        }else{
+            alert('Invalid Phone Number or Password');
+        }
+
+
     })
